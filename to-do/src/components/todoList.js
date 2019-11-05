948c4b896ce41a2f185feb9266d7fbbb01c2ae4d
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-11-05 16:36:41 -0500

    working on debuggin, and tryin to display items

--- to-do/src/components/todoList.js
+++ to-do/src/components/todoList.js
@@ -2,7 +2,31 @@
 import React, { useReducer, useState } from 'react';
 
 import { todoReducer, initialState } from '../reducers/todoReducer';
+import todoItem from './todoItem';
+
 
 const todoList = () => {
     const [state, dispatch] = useReducer(todoReducer, initialState);
+
+    const [additionalItem, setAdditionalItem] = useState(
+        {
+            item: '',
+            completed: false,
+            id: Date.now()
+        });
+    //handle event
+    const handleChanges = (e) => {
+        setAdditionalItem({ ...additionalItem, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch({ type: 'addItem', playload: additionalItem });
+        setAdditionalItem({
+            item: '',
+            completed: false,
+            id: Date.now()
+        })
+    }
+
 }
